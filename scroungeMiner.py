7b5a0f7230190e5a772a6ad02cbd8180e0b20ee2
dancingborg <dancingborg@users.noreply.github.com>
2018-05-26 22:02:06 +0800

    miner works when tested and working with 3 zeroes

--- scroungeMiner.py
+++ scroungeMiner.py
@@ -6,15 +6,13 @@ import hashlib
 
 
 def _timestamp():
-    """Return the current date and time in blockchain format."""
+    """Return the current date and time."""
     # Calculate current date and time
     now = datetime.datetime.now()
     # Convert now into a string
     timestring = str(now)
-    # Create a string correctly formatted for the file
-    stamp = "TIME  = " + timestring
     # Write the correctly formatted string
-    return stamp
+    return timestring
 
 
 # Checks to see if previous blocks. If not start first block.
@@ -45,9 +43,10 @@ def _checkFirst():
         # Print the index string
         print(index)
         timestamp = _timestamp()
-        BCfile.write(timestamp + "\n")
+        timestampFormat = "TIME  = " + timestamp
+        BCfile.write(timestampFormat + "\n")
         # Print the correctly formatted string
-        print(timestamp)
+        print(timestampFormat)
         # The specified arbitrary data of the first block
         data = "first block"
         # The data formatted correctly for the file
@@ -69,7 +68,7 @@ def _checkFirst():
         BCfile.write(nonceString + "\n")
         print(nonceString)
         # End of block furniture
-        end = "-----END BLOCK-----\n\n"
+        end = "-----END BLOCK-----\n"
         BCfile.write(end)
         print(end)
     # Regardless of new file or not. Close at the end.
@@ -154,6 +153,15 @@ def _hashPrevBlock():
     lines = lines[:7]
     # Un-reverse the order of these last 7 lines back to normal
     lines = reversed(lines)
+    # Recast as list
+    lines = list(lines)
+    # Strip out the beginning and ending furniture
+    lines = lines[1:6]
+    counter = 0
+    for line in lines:
+        lines[counter] = line.strip()
+        lines[counter] = line[8:]
+        counter += 1
     # Start a new sha256 object
     hash = hashlib.sha256()
     # Iterate through this list of the last block
@@ -192,79 +200,100 @@ def _getNewData():
 def _newBlock():
     """Create a new block on the blockchain."""
     nonce = 0
-    print("\nCalculating new block up to nonce of 50 000.\n")
-    begin = "-----BEGIN BLOCK-----"
+    # Beginning furniture for _hashPrevBlock
+    beginFormat = "-----BEGIN BLOCK-----"
     # Get the last index from the blockchain filename
     index = _getNewIndex()
-    index = "INDEX = " + str(index)
+    # Formatted string for writing
+    indexFormat = "INDEX = " + str(index)
     # Generate a timestamp for this block
     time = _timestamp()
+    # Formatted string for writing
+    timeFormat = "TIME  = " + time
     # Gather the last transaction as the block data
     data = _getNewData()
-    data = "DATA  = " + data
-    lastHash = _hashPrevBlock()
-    lastHash = "PREV  = " + lastHash.hexdigest()
-    end = "-----END BLOCK------"
+    # Formatted string for writing
+    dataFormat = "DATA  = " + data
+    # SHA256 of previous block contents (no furniture)
+    prevHash = _hashPrevBlock()
+    prevHashString = prevHash.hexdigest()
+    # Formatted string for writing
+    lastHashFormat = "PREV  = " + prevHashString
+    # Block ending furniture for writing
+    endFormat = "-----END BLOCK------"
     while True:
-        nonceString = "NONCE = " + str(nonce)
-        # Reset the variable
+        # First convert the index to a string (rest are strings)
+        indexString = str(index)
+        # Encode the strings to bytes for hashing
+        indexBytes = indexString.encode()
+        timeBytes = time.encode()
+        dataBytes = data.encode()
+        hashBytes = prevHashString.encode()
+        # Create a block variable to hold the whole block
+        block = indexBytes + timeBytes + dataBytes + hashBytes
+        # Reset the hash variable
         newhash = None
+        # Start the hash variable
         newhash = hashlib.sha256()
-        # Add the various parts of the block in
-        newhash.update(begin.encode())
-        newhash.update(index.encode())
-        newhash.update(time.encode())
-        newhash.update(data.encode())
-        newhash.update(lastHash.encode())
-        newhash.update(nonceString.encode())
-        newhash.update(end.encode())
+        # Add the block to be hashed
+        newhash.update(block)
+        # Convert the nonce to a string
+        nonceString = str(nonce)
+        # Prepare the nonce for adding to the hash
+        nonceBytes = nonceString.encode()
+        # Update block by concatenating the nonceBytes
+        newhash.update(nonceBytes)
         # Create a string of the hash
-        first14 = newhash.hexdigest()
-        # Get the first 14 characters
-        first14 = first14[0: 14]
+        digest = newhash.hexdigest()
+        # Get the first 14 characters of the hash digest
+        first14 = digest[0:14]
         # Fourteen zeroes for comparison
         fourteenZeros = "00000000000000"
         if (first14 == fourteenZeros):
+            # Prepare the nonce for writing to file
+            nonceFormat = "NONCE = " + str(nonce)
             # Open the blockchain file for writing
             BCfile = open(blockchain, mode="a+")
-            BCfile.write(begin+"\n")
-            BCfile.write(index+"\n")
-            BCfile.write(time+"\n")
-            BCfile.write(data+"\n")
-            BCfile.write(lastHash+"\n")
-            BCfile.write(nonceString+"\n")
-            BCfile.write(end+"\n\n")
+            BCfile.write(beginFormat+"\n")
+            BCfile.write(indexFormat+"\n")
+            BCfile.write(timeFormat+"\n")
+            BCfile.write(dataFormat+"\n")
+            BCfile.write(lastHashFormat+"\n")
+            BCfile.write(nonceFormat+"\n")
+            BCfile.write(endFormat+"\n\n")
             BCfile.close()
             # Print the new block to the terminal
-            print(begin)
-            print(index)
-            print(time)
-            print(data)
-            print(lastHash)
-            print(nonceString)
-            print(end)
+            print(beginFormat)
+            print(indexFormat)
+            print(timeFormat)
+            print(dataFormat)
+            print(lastHashFormat)
+            print(nonceFormat)
+            print(endFormat+"\n")
             # Break out of the infinite while loop
             break
-        # Per instructions call it quits at nonce value 50 000
+        # Per instructions call it quits at nonce value of 50 000
         if (nonce >= 50000):
+            # Prepare the nonce for writing to file
+            nonceFormat = "NONCE = " + str(nonce)
             # Open the blockchain file for writing
             BCfile = open(blockchain, mode="a+")
-            BCfile.write(begin+"\n")
-            BCfile.write(index+"\n")
-            BCfile.write(time+"\n")
-            BCfile.write(data+"\n")
-            BCfile.write(lastHash+"\n")
-            BCfile.write(nonceString+"\n")
-            BCfile.write(end+"\n\n")
+            BCfile.write(beginFormat+"\n")
+            BCfile.write(indexFormat+"\n")
+            BCfile.write(timeFormat+"\n")
+            BCfile.write(dataFormat+"\n")
+            BCfile.write(lastHashFormat+"\n")
+            BCfile.write(nonceFormat+"\n")
+            BCfile.write(endFormat+"\n\n")
             BCfile.close()
             # Print the new block to the terminal
-            print(begin)
-            print(index)
-            print(time)
-            print(data)
-            print(lastHash)
-            print(nonceString)
-            print(end)
+            print(beginFormat)
+            print(indexFormat)
+            print(timeFormat)
+            print(dataFormat)
+            print(lastHashFormat)
+            print(nonceFormat)
+            print(endFormat+"\n")
             # Break out of the infinite while loop
             break
         nonce += 1
@@ -295,7 +324,7 @@ try:
         # Update the last known ledger state to the one used
         lastLedger = currentLedger
         # Wait for fives seconds before repeating infinite loop
-        time.sleep(5)
+        time.sleep(1)
 except KeyboardInterrupt:
     # Catch keyboard interrupts and close cleanly
     quit()
