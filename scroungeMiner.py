b7b89f2ed5c89fdee6d11c485c8ef2aebc39b74d
dancingborg <dancingborg@users.noreply.github.com>
2018-05-26 14:13:19 +0800

    completed new index calculation and converted timestamp to own function

--- scroungeMiner.py
+++ scroungeMiner.py
@@ -5,8 +5,21 @@ import datetime
 import hashlib
 
 
+def _timestamp():
+    # Calculate current date and time
+    now = datetime.datetime.now()
+    # Convert now into a string
+    timestring = str(now)
+    # Create a string correctly formatted for the file
+    stamp = "TIME  = " + timestring
+    # Write the correctly formatted string
+    return stamp
+
+
 # Checks to see if previous blocks. If not start first block.
 def _checkFirst():
+    # Open filename, create if need be. Set stream at EOF
+    BCfile = open(blockchain, mode="a+")
     # Variable outside scope of iterator through file lines
     isFirst = True
     # Beginning of block furniture to look for
@@ -29,13 +42,7 @@ def _checkFirst():
         BCfile.write(index + "\n")
         # Print the index string
         print(index)
-        # Calculate current date and time
-        now = datetime.datetime.now()
-        # Convert now into a string
-        timestring = str(now)
-        # Create a string correctly formatted for the file
-        timestamp = "TIME  = " + timestring
-        # Write the correctly formatted string
+        timestamp = _timestamp()
         BCfile.write(timestamp + "\n")
         # Print the correctly formatted string
         print(timestamp)
@@ -63,6 +70,76 @@ def _checkFirst():
         end = "-----END BLOCK-----\n\n"
         BCfile.write(end)
         print(end)
+    # Regardless of new file or not. Close at the end.
+    BCfile.close()
+
+
+def _ledgerHash():
+    # Open the ledger file, creating it if needed
+    Lfile = open(ledger, mode="a+")
+    # Start reading the file from the Start
+    Lfile.seek(0)
+    # Start a hash object
+    hash = hashlib.sha256()
+    # Set a 64kb buffer in case file is huge!
+    bufferSize = 65536
+    # Infinite loop through file broken if no data is read
+    while True:
+        # Read the buffer
+        chunk = Lfile.read(bufferSize)
+        if not chunk:
+            break
+        # Update the hash with the chunk as bytes
+        hash.update(chunk.encode())
+    # Close the file when done
+    Lfile.close()
+    # Return the hash of the ledger file
+    return hash
+
+# Read the last known index from the blockchain file
+
+
+def _getNewIndex():
+    # Open the blockchain file
+    blockfile = open(blockchain, mode="a+")
+    # Seek to the start of the file
+    blockfile.seek(0)
+    # Convert the file into a list of lines
+    lines = list(blockfile)
+    # Reverse the order of the lines
+    lines = reversed(lines)
+    # Strip all lines of leading and trailing whitespace & EOL
+    lines = (line.strip() for line in lines)
+    # Remove all blank lines
+    lines = (line for line in lines if line)
+    # Start a counter for the for loop
+    counter = 0
+    # Iterate through the lines
+    for line in lines:
+        # Increment the counter each time
+        counter += 1
+        # At the sixth line, do the following
+        if counter == 6:
+            # Return an integer conversion of the number
+            # Take all characters including and after the 8th
+            # This accounts for multi-digit numbers
+            lastIndex = int(line[8:])
+            # Increment the existing index to get the new index
+            return lastIndex + 1
+            # Stop the for loop
+            break
+    # Cleanly close the file before ending this function
+    blockfile.close()
+
+
+def _newBlock():
+    print("Ledger change detected!")
+    # Get the last index from the blockchain filename
+    print("-----BEGIN BLOCK-----")
+    index = _getNewIndex()
+    print("INDEX = " + str(index))
+    time = _timestamp()
+    print("TIME  = " + time)
 
 
 # Main program loop
@@ -74,16 +151,24 @@ try:
     print("This program works alongside \"scroungeLedger.py\".")
     print("This program checks \"ledger.txt\" for transactions.")
     print("This program adds blocks to \"blockchain.txt\".")
-    # Infinite loop unless "no" or KeyboardInterrupt
+    # Set filename as a variable for easy change if needed
+    blockchain = "blockchain.txt"
+    ledger = "ledger.txt"
+    # Maintain state of ledger file in this variable
+    lastLedger = _ledgerHash()
+    # Infinite loop unless KeyboardInterrupt
     while True:
-        # Set filename as a variable for easy change if needed
-        filename = "blockchain.txt"
-        # Open filename, create if need be. Set stream at EOF
-        BCfile = open(filename, mode="a+")
+        # Check if a previous block exists in the blockchain file
         _checkFirst()
-        BCfile.close()
+        # Update the state of the ledger with current state
+        currentLedger = _ledgerHash()
+        # Let the user know the current state of the ledger
+        if (currentLedger.hexdigest() != lastLedger.hexdigest()):
+            _newBlock()
+        # Update the last known ledger state to the one used
+        lastLedger = currentLedger
+        # Wait for fives seconds before repeating infinite loop
         time.sleep(5)
 except KeyboardInterrupt:
     # Catch keyboard interrupts and close cleanly
-    BCfile.close()
     quit()
